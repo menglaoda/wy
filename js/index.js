b23d7217214161e4903bc60bba662c6a9bd354a1
mengshiyu <1415940875@qq.com>
2017-01-12 16:38:54 +0800

    登录页面

--- js/index.js
+++ js/index.js
@@ -11,12 +11,26 @@
 				//ie8及以下
 				return obj.currentStyle[attr];
 			}
+				var door1 = document.querySelector(".door1");
+				var door2 = document.querySelector(".door2");
+				var door3 = document.querySelector(".door3");
+				var door3inp = document.querySelectorAll(".door3 input");
+				var door3span = document.querySelector(".door3 span");
 				var box = document.querySelector(".box");
 				var owidth = window.innerWidth;
 				var oheight =window.innerHeight;
-				box.style.height = oheight+"px";
+				door1.style.height = oheight+"px";door1.style.width = parseInt(owidth/2)+"px";
+				door2.style.height = oheight+"px";
+				box.style.height = oheight+"px";door2.style.width = parseInt(owidth/2)+"px";door2.style.left = parseInt(owidth/2)+"px";
 				console.log(owidth);
 				console.log(oheight);
+				door3span.onclick = function(){
+					if(door3inp[0].value=="逗比" && door3inp[1].value=="520"){
+						door3.style.display = "none";
+						startMove(door1,"left",-parseInt(owidth/2));
+						startMove(door2,"left",owidth);
+					}
+				}
 				
 				var navli = document.querySelectorAll(".nav li");
 				var box1section = document.querySelectorAll(".box1 section");
